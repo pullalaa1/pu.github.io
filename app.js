4a3e728d13bfffddabdd02d0b098698479661e8a
pullalaa1 <142795621+pullalaa1@users.noreply.github.com>
2023-08-21 16:37:02 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand()
+
+tg.MainButton.textColor = "FFFFFF";
+tg.MainButton.color = "#2cab37";
+
+leg item = "";
+
+let name = document.getElementById("name")
+let age = document.getElementById("age")
+let gender = document.getElementById("gender")
+
+let btn_register = document.getElementById("btn_register")
+
