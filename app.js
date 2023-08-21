7d0757877030d9182e6144647d67a5f9fd2ddc0e
pullalaa1 <142795621+pullalaa1@users.noreply.github.com>
2023-08-21 20:52:35 +0600

    Update app.js

--- app.js
+++ app.js
@@ -32,3 +32,8 @@ btn.addEventListener('click', function() {
     tg.sendData("Click");
     console.log('test2');
 });
+
+Telegram.WebApp.onEvent('mainButtonClicked', function(){
+      tg.sendData("some string that we need to send"); 
+      //при клике на основную кнопку отправляем данные в строковом виде
+   });
