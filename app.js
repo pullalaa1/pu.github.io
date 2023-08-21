a7948e88025e1d3eeaee4c2a389e1354a74288a4
pullalaa1 <142795621+pullalaa1@users.noreply.github.com>
2023-08-21 16:58:20 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 let tg = window.Telegram.WebApp;
 
-tg.expand()
+tg.expand();
 
 tg.MainButton.textColor = "FFFFFF";
 tg.MainButton.color = "#2cab37";
 
-leg item = "";
+let name = document.getElementById("name");
+let age = document.getElementById("age");
+let gender = document.getElementById("gender");
 
-let name = document.getElementById("name")
-let age = document.getElementById("age")
-let gender = document.getElementById("gender")
+let btn_register = document.getElementById("btn_register");
 
-let btn_register = document.getElementById("btn_register")
+// Создание обработчика для кнопки регистрации
+btn_register.addEventListener("click", function() {
+    let nameValue = name.value;
+    let ageValue = age.value;
+    let genderValue = gender.value;
 
+    // Отправка данных боту
+    tg.sendData(nameValue);
+    tg.sendData(ageValue);
+    tg.sendData(genderValue);
+});
